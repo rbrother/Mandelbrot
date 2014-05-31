afa43036e42d21b89c88a191d7f3dab4dd442cf2
Robert Brotherus <robert@iki.fi>
2014-05-31 21:12:56 +0300

    Full-screen display with non-square aspect ratio

--- ComplexFunctionWindow.cs
+++ ComplexFunctionWindow.cs
@@ -26,36 +26,36 @@ abstract public class ComplexFunctionWindow : Window {
     Image image;
     WriteableBitmap Bmp;
     Complex Center { get; set; }
-    double RealSize = 4.0;
-    readonly int BLOCK_SIZE = 100;
+    double RealHeight = 4.0;
+    readonly int BlockPixelSize = 100;
     private CancellationTokenSource _cancellationSource = null;
     private CancellationToken _cancellationToken;
 
-    public int Pixels {
-        get { return Convert.ToInt32(SystemParameters.PrimaryScreenHeight) - 100; }
-    }
+    double RealWidth { get { return RealHeight * PixelWidth / PixelHeight; } }
+
+    public int PixelWidth { get { return Convert.ToInt32( SystemParameters.PrimaryScreenWidth ); } }
+    public int PixelHeight { get { return Convert.ToInt32( SystemParameters.PrimaryScreenHeight ); } }
 
     public ComplexFunctionWindow( ) {
-        this.Left = 20.0;
-        this.Top = 20.0;
+        this.WindowState = System.Windows.WindowState.Maximized;
+        this.WindowStyle = System.Windows.WindowStyle.None;
+        this.Topmost = true;
         Center = new Complex( 0.0, 0.0 );
-        SizeToContent = SizeToContent.WidthAndHeight;
         image = new Image( );
-        image.Width = Pixels;
-        image.Height = Pixels;
         this.Content = image;
-        Bmp = new WriteableBitmap( Pixels, Pixels, 96, 96, PixelFormats.Bgra32, null );
-        image.Source = Bmp;
         image.MouseWheel += image_MouseWheel;
-        image.MouseUp += image_MouseUp;
+        image.MouseLeftButtonUp += image_MouseUp;
+        image.MouseRightButtonUp += image_MouseRightButtonUp;
+        Bmp = new WriteableBitmap( PixelWidth, PixelHeight, 96, 96, PixelFormats.Bgra32, null );
+        image.Source = Bmp;
         Draw( );
     }
 
-    private Complex Origin { get { return new Complex( Center.Real - RealSize * 0.5, Center.Imaginary - RealSize * 0.5 ); } }
+    private Complex Origin { get { return new Complex( Center.Real - RealWidth * 0.5, Center.Imaginary - RealHeight * 0.5 ); } }
 
-    private double Step { get { return RealSize / Pixels; } }
+    private double Step { get { return RealHeight / PixelHeight; } }
 
-    private double BlockRealSize { get { return BLOCK_SIZE * Step; } }
+    private double BlockRealSize { get { return BlockPixelSize * Step; } }
 
     void image_MouseUp( object sender, System.Windows.Input.MouseButtonEventArgs e ) {
         var pos = e.MouseDevice.GetPosition( image );
@@ -63,36 +63,48 @@ abstract public class ComplexFunctionWindow : Window {
         Draw( );
     }
 
+    void image_MouseRightButtonUp( object sender, System.Windows.Input.MouseButtonEventArgs e ) {
+        Zoom( zoomIn: true, Scale: 1.5 );
+    }
+
     void image_MouseWheel( object sender, System.Windows.Input.MouseWheelEventArgs e ) {
+        var zoomIn = e.Delta > 0;
         var Scale = 1.0 + 0.002 * Math.Abs( e.Delta );
         if ( e.Delta < 0 ) Scale = 1.0 / Scale;
+        Zoom( zoomIn, Scale );
+    }
+
+    private void Zoom( bool zoomIn, double Scale ) {
         var InverseScale = 1.0 / Scale;
         var transformed = new TransformedBitmap( Bmp, new ScaleTransform( Scale, Scale ) );
-        var buffer = new byte[Pixels * Pixels * 4];
-        if ( e.Delta > 0 ) {
-            var offset = Convert.ToInt32( Pixels * ( Scale - 1 ) * 0.5 );            
-            transformed.CopyPixels( new Int32Rect( offset, offset, Pixels, Pixels ), buffer, Pixels * 4, 0 );
-            Bmp.WritePixels( new Int32Rect( 0, 0, Pixels, Pixels ), buffer, Pixels * 4, 0 );
+        var buffer = new byte[PixelWidth * PixelHeight * 4];
+        if ( zoomIn ) {
+            var offsetX = Convert.ToInt32( PixelWidth * ( Scale - 1 ) * 0.5 );
+            var offsetY = Convert.ToInt32( PixelHeight * ( Scale - 1 ) * 0.5 );
+            transformed.CopyPixels( new Int32Rect( offsetX, offsetY, PixelWidth, PixelHeight ), buffer, PixelWidth * 4, 0 );
+            Bmp.WritePixels( new Int32Rect( 0, 0, PixelWidth, PixelHeight ), buffer, PixelWidth * 4, 0 );
         } else {
-            var offset = Convert.ToInt32( Pixels * ( 1 - Scale ) * 0.5 );
-            transformed.CopyPixels( new Int32Rect( 0, 0, transformed.PixelWidth, transformed.PixelHeight ), buffer, Pixels * 4, 0 );
-            Bmp.WritePixels( new Int32Rect( 0, 0, Pixels, Pixels ), new byte[Pixels * Pixels * 4], Pixels * 4, 0 ); // Clear
-            Bmp.WritePixels( new Int32Rect( offset, offset, transformed.PixelWidth, transformed.PixelWidth ), buffer, Pixels * 4, 0 );
+            var offsetX = Convert.ToInt32( PixelWidth * ( 1 - Scale ) * 0.5 );
+            var offsetY = Convert.ToInt32( PixelHeight * ( 1 - Scale ) * 0.5 );
+            transformed.CopyPixels( new Int32Rect( 0, 0, (int)transformed.Width, (int)transformed.Height ), buffer, PixelWidth * 4, 0 );
+            Bmp.WritePixels( new Int32Rect( 0, 0, PixelWidth, PixelHeight ), new byte[PixelWidth * PixelHeight * 4], PixelWidth * 4, 0 ); // Clear
+            Bmp.WritePixels( new Int32Rect( offsetX, offsetY, (int)transformed.Width, (int)transformed.Height ), buffer, PixelWidth * 4, 0 );
         }
-        RealSize = RealSize * InverseScale;
-        this.Title = string.Format( "Scale {0}", RealSize );
+        RealHeight = RealHeight * InverseScale;
+        this.Title = string.Format( "Scale {0}", RealHeight );
         Draw( );
     }
 
     private void Draw( ) {
+        int BlockCountX = PixelWidth / BlockPixelSize;
+        int BlockCountY = PixelHeight / BlockPixelSize;
         if ( _cancellationSource != null ) _cancellationSource.Cancel( );
         _cancellationSource = new CancellationTokenSource( );
         _cancellationToken = _cancellationSource.Token;
-        var block_count = Pixels / BLOCK_SIZE;
-        for ( int y_block = 0; y_block < block_count; ++y_block ) {
-            for ( int x_block = 0; x_block < block_count; ++x_block ) {
+        for ( int y_block = 0; y_block < BlockCountY; ++y_block ) {
+            for ( int x_block = 0; x_block < BlockCountX; ++x_block ) {
                 var blockOrigin = Origin + new Complex( x_block, y_block ) * BlockRealSize;
-                var blockParams = new BlockParams { Origin = blockOrigin, PixelOffsetX = x_block * BLOCK_SIZE, PixelOffsetY = y_block * BLOCK_SIZE };
+                var blockParams = new BlockParams { Origin = blockOrigin, PixelOffsetX = x_block * BlockPixelSize, PixelOffsetY = y_block * BlockPixelSize };
                 Task.Factory.StartNew( new Action( ( ) => DrawBlock( blockParams ) ), _cancellationToken );
             }
         }
@@ -100,16 +112,16 @@ abstract public class ComplexFunctionWindow : Window {
 
     private void DrawBlock( BlockParams par ) {
         if ( _cancellationToken.IsCancellationRequested ) return;
-        byte[] buffer = new byte[BLOCK_SIZE * BLOCK_SIZE * 4];
-        for ( int y = 0; y < BLOCK_SIZE; ++y ) {
-            for ( int x = 0; x < BLOCK_SIZE; ++x ) {
+        byte[] buffer = new byte[BlockPixelSize * BlockPixelSize * 4];
+        for ( int y = 0; y < BlockPixelSize; ++y ) {
+            for ( int x = 0; x < BlockPixelSize; ++x ) {
                 if ( _cancellationToken.IsCancellationRequested ) return;
                 var pixel_x = x + par.PixelOffsetX;
                 var pixel_y = y + par.PixelOffsetY;
-                if ( pixel_x >= Pixels || pixel_y >= Pixels ) continue;
+                if ( pixel_x >= PixelWidth || pixel_y >= PixelHeight ) continue;
                 var c = par.Origin + new Complex( x * Step, y * Step );
                 var color = FunctionColor(c);
-                var bufferOffset = ( y * BLOCK_SIZE + x ) * 4;
+                var bufferOffset = ( y * BlockPixelSize + x ) * 4;
                 buffer[bufferOffset] = color.B;
                 buffer[bufferOffset + 1] = color.G;
                 buffer[bufferOffset + 2] = color.R;
@@ -117,7 +129,9 @@ abstract public class ComplexFunctionWindow : Window {
             }
         }
         Dispatcher.BeginInvoke( new Action( ( ) => {
-            Bmp.WritePixels( new Int32Rect( 0, 0, BLOCK_SIZE, BLOCK_SIZE ), buffer, 4 * BLOCK_SIZE, par.PixelOffsetX, par.PixelOffsetY );
+            if ( par.PixelOffsetX + BlockPixelSize <= Bmp.Width && par.PixelOffsetY + BlockPixelSize <= Bmp.Height ) {
+                Bmp.WritePixels( new Int32Rect( 0, 0, BlockPixelSize, BlockPixelSize ), buffer, 4 * BlockPixelSize, par.PixelOffsetX, par.PixelOffsetY );
+            }
         } ), DispatcherPriority.Background );
     }
 
--- MandelbrotWindow.cs
+++ MandelbrotWindow.cs
@@ -6,12 +6,12 @@ public class MandelbrotWindow : ComplexFunctionWindow {
 
     protected override Color FunctionColor( Complex c ) {
         var z = new Complex( 0.0, 0.0 );
-        int iteration = 0;
-        int max_iteration = 512;
-        while ( iteration < max_iteration ) {
+        int count = 0;
+        int maxCount = 512;
+        while ( count < maxCount ) {
             z = z * z + c;
-            if ( z.MagnitudeSqr() > 4 ) return ColorMap(iteration);
-            iteration += 1;
+            if ( z.MagnitudeSqr() > 4 ) return ColorMap(count);
+            count += 1;
         }
         return Colors.Black;
     }
